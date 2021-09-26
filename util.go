67c75bc0fc60d35631dd4b8ca2bda6bcef7f6c28
Burak Köken <burakkokenn@gmail.com>
2021-09-26 12:10:52 +0200

    Add GoModDir utility function
    Fixes #7

--- util.go
+++ util.go
@@ -1,6 +1,9 @@
 package marker
 
 import (
+	"fmt"
+	"golang.org/x/tools/go/packages"
+	"os"
 	"strings"
 	"unicode"
 )
@@ -25,4 +28,37 @@ func LowerCamelCase(str string) string {
 		return r
 	}, str)
 
+}
+
+// GoModDir returns the directory of go.mod file.
+func GoModDir() (string, error) {
+	var wd string
+	var err error
+	wd, err = os.Getwd()
+
+	if err != nil {
+		return "", fmt.Errorf("wtf - what a terrible failure! : %s", err.Error())
+	}
+
+	config := &packages.Config{}
+	config.Mode |= packages.NeedModule
+
+	var pkgs []*packages.Package
+	pkgs, err = packages.Load(config, wd)
+
+	if err != nil {
+		return "", fmt.Errorf("an error occurred : %s", err.Error())
+	}
+
+	if pkgs == nil || len(pkgs) == 0 {
+		return "", fmt.Errorf("package not found for the directory %s", wd)
+	}
+
+	pkg := pkgs[0]
+
+	if pkg.Module == nil {
+		return "", fmt.Errorf("go.mod does not exist for the directory %s", wd)
+	}
+
+	return pkg.Module.Dir, nil
 }
