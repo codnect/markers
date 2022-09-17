747fc289a41b835c715e184dd9182e07305fa684
Burak Koken <burakkokenn@gmail.com>
2022-09-18 02:50:42 +0300

    Improve unit test coverage

--- collector_test.go
+++ collector_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestCollector_Collect(t *testing.T) {
-	result, _ := packages.LoadPackages("../test/package1")
-	pkg, _ := result.Lookup("../test/package1")
+	result, _ := packages.LoadPackages("github.com/procyon-projects/marker/test/package1")
+	pkg, _ := result.Lookup("github.com/procyon-projects/marker/test/package1")
 
 	registry := NewRegistry()
 	collector := NewCollector(registry)
