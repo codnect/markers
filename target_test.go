0c93c587d7c6aa804a0fba6e6c25e2dcf7f730a5
Burak Koken <burakkokenn@gmail.com>
2022-09-23 20:13:50 +0300

    Improve unit test coverage

---
+++ target_test.go
@@ -0,0 +1,26 @@
+package marker
+
+import (
+	"github.com/stretchr/testify/assert"
+	"go/ast"
+	"testing"
+)
+
+func TestFindTargetLevelFromNode(t *testing.T) {
+	assert.Equal(t, StructTypeLevel, FindTargetLevelFromNode(&ast.TypeSpec{
+		Type: &ast.StructType{},
+	}))
+	assert.Equal(t, InterfaceTypeLevel, FindTargetLevelFromNode(&ast.TypeSpec{
+		Type: &ast.InterfaceType{},
+	}))
+	assert.Equal(t, FieldLevel, FindTargetLevelFromNode(&ast.Field{}))
+	assert.Equal(t, InterfaceMethodLevel, FindTargetLevelFromNode(&ast.Field{
+		Type: &ast.FuncType{},
+	}))
+	assert.Equal(t, StructMethodLevel, FindTargetLevelFromNode(&ast.FuncDecl{
+		Recv: &ast.FieldList{},
+	}))
+	assert.Equal(t, FunctionLevel, FindTargetLevelFromNode(&ast.FuncDecl{}))
+	assert.Equal(t, PackageLevel, FindTargetLevelFromNode(&ast.Package{}))
+	assert.Equal(t, InvalidLevel, FindTargetLevelFromNode(nil))
+}
